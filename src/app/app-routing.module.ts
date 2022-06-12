695cba943609e6b2bf1d19e98059bedc6db49059
Emanuel Dinardo <dinardoema@gmail.com>
2022-06-12 12:27:49 -0300

    Front corion proyect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { EditComponent } from './components/edit/edit.component';
+import { AddComponent } from './components/add/add.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { AuthGuard } from './services/auth.guard';
+import { UserComponent } from './components/user/user.component';
+import { CargaComponent } from './components/carga/carga.component';
+import { HistorialComponent } from './components/historial/historial.component';
+import { HCustomComponent } from './components/h-custom/h-custom.component';
+import { HConsumoComponent } from './components/h-consumo/h-consumo.component';
+import { HHoraComponent } from './components/h-hora/h-hora.component';
+import { HGeneralComponent } from './components/h-general/h-general.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ComisariatoAccionesComponent } from './components/comisariato-acciones/comisariato-acciones.component';
+import { ClimaComponent } from './components/clima/clima.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'fligths', component: SidenavComponent},
+  { path: 'main', component: MainComponent, canActivate: [AuthGuard]},
+  { path: 'edit', component: EditComponent,canActivate: [AuthGuard]},
+  { path: 'add', component: AddComponent,canActivate: [AuthGuard]},
+  { path: 'user', component: UserComponent,canActivate: [AuthGuard]},
+  { path: 'carga', component: CargaComponent,canActivate: [AuthGuard]},
+  { path: 'historial', component: HistorialComponent,canActivate: [AuthGuard]},
+  { path: 'custom', component: HCustomComponent,canActivate: [AuthGuard]},
+  { path: 'consumo', component: HConsumoComponent,canActivate: [AuthGuard]},
+  { path: 'general', component: HGeneralComponent,canActivate: [AuthGuard]},
+  { path: 'hora', component: HHoraComponent,canActivate: [AuthGuard]},
+  { path: 'reporte-c', component: ReportesComponent,canActivate: [AuthGuard]},
+  { path: 'dashboard', component: DashboardComponent,canActivate: [AuthGuard]},
+  { path: 'comisariato', component: ComisariatoAccionesComponent,canActivate: [AuthGuard]},
+  { path: 'clima', component: ClimaComponent,canActivate: [AuthGuard]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,109 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ObjToArrayPipe } from './components/pipes/ObjToArray.pipe';
+import { EditComponent } from './components/edit/edit.component';
+import { AddComponent } from './components/add/add.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { AuthService } from './services/auth.service';
+import { AuthGuard } from './services/auth.guard';
+import { RegistroVueloComponent } from './components/registro-vuelo/registro-vuelo.component';
+import { UserComponent } from './components/user/user.component';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { HistorialComponent } from './components/historial/historial.component';
+import { CargaComponent } from './components/carga/carga.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTree, MatTreeModule} from '@angular/material/tree';
+import { HGeneralComponent } from './components/h-general/h-general.component';
+import { HHoraComponent } from './components/h-hora/h-hora.component';
+import { HConsumoComponent } from './components/h-consumo/h-consumo.component';
+import { HCustomComponent } from './components/h-custom/h-custom.component';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ReporteCargaComponent } from './components/reporte-carga/reporte-carga.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetalleComponent } from './components/detalle/detalle.component';
+import { ComisariatoComponent } from './components/comisariato/comisariato.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { ComisariatoAccionesComponent } from './components/comisariato-acciones/comisariato-acciones.component';
+import { ReportesComponent } from './components/reportes/reportes.component';
+import { ReporteAeronaveComponent } from './components/reporte-aeronave/reporte-aeronave.component';
+import { ClimaComponent } from './components/clima/clima.component';
+import { ClimaDetalleComponent } from './components/clima-detalle/clima-detalle.component';
+import { ClimaService } from './services/clima.service';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ObjToArrayPipe,
+    EditComponent,
+    AddComponent,
+    SidenavComponent,
+    RegistroVueloComponent,
+    UserComponent,
+    HistorialComponent,
+    CargaComponent,
+    HGeneralComponent,
+    HHoraComponent,
+    HConsumoComponent,
+    HCustomComponent,
+    ReporteCargaComponent,
+    DashboardComponent,
+    DetalleComponent,
+    ComisariatoComponent,
+    ComisariatoAccionesComponent,
+    ReportesComponent,
+    ReporteAeronaveComponent,
+    ClimaComponent,
+    ClimaDetalleComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatTableModule,
+    MatDialogModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule,
+    MatPaginatorModule,
+    MatCheckboxModule,
+    MatTreeModule,
+    PdfViewerModule,
+    MatDatepickerModule,
+    NgxMaterialTimepickerModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard, ClimaService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
